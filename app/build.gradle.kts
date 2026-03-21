15c71eabe5634d78889ef3fa34718ec22a4c57ce
nulldio <pengooz@proton.me>
2026-03-21 12:29:08 -0400

    v1.5 - Fix volume step measurement, improve input UX

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.thirtytwo.steps"
         minSdk = 28
         targetSdk = 34
-        versionCode = 5
-        versionName = "1.4"
+        versionCode = 6
+        versionName = "1.5"
     }
 
     if (file("../release-key.jks").exists()) {
