22338176f5de1de28f86e292fad6e7d1743156bc
nulldio <pengooz@proton.me>
2026-03-21 15:27:32 -0400

    Bump to v1.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.thirtytwo.steps"
         minSdk = 26
         targetSdk = 34
-        versionCode = 7
-        versionName = "1.6"
+        versionCode = 8
+        versionName = "1.7"
     }
 
     if (file("../release-key.jks").exists()) {
