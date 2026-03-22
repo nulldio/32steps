2b1bc8b781ce85c8ccd17900597ac2c7af41937b
nulldio <pengooz@proton.me>
2026-03-22 01:52:47 -0400

    v2.0.3 - Fix sound profile persistence

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.thirtytwo.steps"
         minSdk = 26
         targetSdk = 34
-        versionCode = 12
-        versionName = "2.0.2"
+        versionCode = 13
+        versionName = "2.0.3"
     }
 
     androidResources {
