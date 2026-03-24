41113b1bb36f2bc84bd8b840ba79dff04788bc9f
nulldio <pengooz@proton.me>
2026-03-24 13:19:03 -0400

    v2.0.9 - Call label and smart dropdown during calls

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.thirtytwo.steps"
         minSdk = 26
         targetSdk = 34
-        versionCode = 18
-        versionName = "2.0.8"
+        versionCode = 19
+        versionName = "2.0.9"
     }
 
     androidResources {
