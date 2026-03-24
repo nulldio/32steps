f0a2d2591fad79188a53d89692621bf9edab1126
nulldio <pengooz@proton.me>
2026-03-23 20:45:09 -0400

    v2.0.6 - Draggable overlay slider, hide notification from lock screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.thirtytwo.steps"
         minSdk = 26
         targetSdk = 34
-        versionCode = 15
-        versionName = "2.0.5"
+        versionCode = 16
+        versionName = "2.0.6"
     }
 
     androidResources {
