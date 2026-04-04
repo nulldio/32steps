194db93f0688f2361dbcab1fee95b5296514f149
nulldio <pengooz@proton.me>
2026-04-04 11:48:40 -0400

    Bump version to 2.2.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.thirtytwo.steps"
         minSdk = 26
         targetSdk = 34
-        versionCode = 25
-        versionName = "2.2.1"
+        versionCode = 26
+        versionName = "2.2.2"
     }
 
     androidResources {
